0433e0116bdec44cff6084f8b8d3c2fd30871bed
Anuj Kumar <anujkumar7676@gmail.com>
2025-08-10 06:35:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase/server/index.ts
+++ src/lib/firebase/server/index.ts
@@ -1,18 +1,12 @@
-import admin from 'firebase-admin';
-import { getApps } from 'firebase-admin/app';
-import dotenv from 'dotenv';
+import * as admin from 'firebase-admin';
 
-dotenv.config();
-
-const serviceAccount = {
-  project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  private_key: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
-};
-
-if (!getApps().length) {
+if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+    }),
   });
 }
 
