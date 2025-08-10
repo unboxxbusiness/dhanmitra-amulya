b60d7ba6bc3a5e88c48b10dc62bfd64384fd5625
Anuj Kumar <anujkumar7676@gmail.com>
2025-08-10 06:26:24 +0000

    Console Error
    Hydration failed because the server rendered HTML didn't

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,15 @@ export default {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
